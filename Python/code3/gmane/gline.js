767c0b00c46ab450facb1b99b27e6ee7994ac833
Prudhvivuda <prudhvi12042001@gmail.com>
2022-11-05 14:16:28 +0530

    initial commit

---
+++ Python/code3/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','ufp.pt','indiana.edu','berkeley.edu','mac.com','uct.ac.za','columbia.edu','unl.edu','mtu.edu'],
+['2005',57,11,10,12,12,12,14,13,12,11],
+['2006',14,10,11,6,5,3,1,1,0,0]
+];
